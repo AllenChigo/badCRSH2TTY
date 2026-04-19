d60e1484c55bc4b8fd6486c94c8ad41a06952699
Buypass07 <idkchigo@gmail.com>
2026-04-19 16:46:17 -0400

    Clean up script by removing usage instructions
    Removed usage instructions and important notes from the script.

--- badCRSH2TTY.sh
+++ badCRSH2TTY.sh
@@ -232,27 +232,3 @@ else
 fi
 
 exit $exit_code
-
-To use this script:
-
-Enable developer mode on your ChromeOS device:
-
-Turn off the ChromeOS device
-Press ESC + REFRESH + POWER buttons
-At the recovery screen, press CTRL+D
-Confirm by pressing ENTER
-Save this script to a file (e.g., chromeos_unenroll.sh)
-
-Make it executable:
-
-bash
-chmod +x chromeos_unenroll.sh
-Run the script:
-
-bash
-./chromeos_unenroll.sh --backup
-Restart the device after the script completes
-
-Important notes:
-
-This script requires ChromeOS developer mode to be enabled
